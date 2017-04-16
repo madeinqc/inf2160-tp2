07fd5c3df601d3ec84c6e12e7be6cd9f0c93b167
Marc-Antoine Sauve <madeinqchw@gmail.com>
2017-04-16 19:59:59 -0400

    Completed the homework
    Signed-off-by: Marc-Antoine Sauve <madeinqchw@gmail.com>

--- main.pl
+++ main.pl
@@ -1,5 +1,3 @@
-:- dynamic(faitsMaisons/6).
-
 meme( anglais, rouge ).
 meme( chien, espagnol ).
 meme( norvegien, 1 ).
@@ -11,225 +9,278 @@ meme( canadien, poire ).
 meme( cafe, verte ).
 meme( lait, 3 ).
 
-memeSansOrdre(X, Y) :-
-  meme(X, Y);
-  meme(Y, X).
-
-memeTest(X, Y) :-
-  memeSansOrdre(X, Y),
-  (
-    maison(X, Y, _, _, _, _);
-    maison(X, _, Y, _, _, _);
-    maison(X, _, _, Y, _, _);
-    maison(X, _, _, _, Y, _);
-    maison(X, _, _, _, _, Y);
-    maison(_, X, Y, _, _, _);
-    maison(_, X, _, Y, _, _);
-    maison(_, X, _, _, Y, _);
-    maison(_, X, _, _, _, Y);
-    maison(_, _, X, Y, _, _);
-    maison(_, _, X, _, Y, _);
-    maison(_, _, X, _, _, Y);
-    maison(_, _, _, X, Y, _);
-    maison(_, _, _, X, _, Y);
-    maison(_, _, _, _, X, Y);
-    maison(Y, X, _, _, _, _);
-    maison(Y, _, X, _, _, _);
-    maison(Y, _, _, X, _, _);
-    maison(Y, _, _, _, X, _);
-    maison(Y, _, _, _, _, X);
-    maison(_, Y, X, _, _, _);
-    maison(_, Y, _, X, _, _);
-    maison(_, Y, _, _, X, _);
-    maison(_, Y, _, _, _, X);
-    maison(_, _, Y, X, _, _);
-    maison(_, _, Y, _, X, _);
-    maison(_, _, Y, _, _, X);
-    maison(_, _, _, Y, X, _);
-    maison(_, _, _, Y, _, X);
-    maison(_, _, _, _, Y, X)
-  ).
-
-memeMaison(X, Y) :-
-  memeSansOrdre(X, Y).
-memeMaison(X, Y) :-
-  memeSansOrdre(X, Z),
-  X \= Z,
-  Z \= Y,
-  memeSansOrdre(Z, Y).
-memeMaison(X, Y) :-
-  voisinSansOrdre(X, Z),
-  position(Y),
-  Y1 is Y + 1,
-  Y2 is Y - 1,
-  (memeSansOrdre(Z, Y1);memeSansOrdre(Z, Y2)).
-
 voisin( celeri, renard ).
 voisin( norvegien, bleu ).
 voisin( kiwi, cheval ).
 voisin( verte, ivoire ).
-voisinSansOrdre(X, Y) :-
-  voisin(X, Y);
-  voisin(Y, X).
 
 droite( verte, ivoire ).
-droite(X, Y) :-
-  droite(X, Z),
-  droite(Z, Y).
 
-gauche(1, 2).
-gauche(2, 3).
-gauche(3, 4).
-gauche(4, 5).
 gauche(X, Y) :-
-  gauche(X, Z),
-  gauche(Z, Y).
+  droite(Y, X).
 
 position(1).
 position(2).
 position(3).
 position(4).
 position(5).
+
 couleur(rouge).
 couleur(jaune).
 couleur(bleu).
 couleur(verte).
 couleur(ivoire).
+
 nationalite(anglais).
 nationalite(espagnol).
 nationalite(norvegien).
 nationalite(ukrainien).
 nationalite(canadien).
+
 animal(chien).
 animal(renard).
 animal(serpent).
 animal(cheval).
 animal(zebre).
+
 breuvage(jus_orange).
 breuvage(the).
 breuvage(cafe).
 breuvage(lait).
 breuvage(eau).
+
 met(kiwi).
 met(celeri).
 met(wasabi).
 met(litchi).
 met(poire).
 
-maison(Position, Couleur, Nationalite, Animal, Breuvage, Met) :-
-  position(Position),
+maison(Couleur, Nationalite, Animal, Breuvage, Met) :-
   couleur(Couleur),
   nationalite(Nationalite),
   animal(Animal),
   breuvage(Breuvage),
   met(Met).
 
-rue([maison(1, CA, NA, AA, BA, MA),
-     maison(2, CB, NB, AB, BB, MB),
-     maison(3, CC, NC, AC, BC, MC),
-     maison(4, CD, ND, AD, BD, MD),
-     maison(5, CE, NE, AE, BE, ME)
-    ]) :-
-  tousDiff([CA, CB, CC, CD, CE]),
-  tousDiff([NA, NB, NC, ND, NE]),
-  tousDiff([AA, AB, AC, AD, AE]),
-  tousDiff([BA, BB, BC, BD, BE]),
-  tousDiff([MA, MB, MC, MD, ME]).
+valeurVersItems(X, X, _, _, _, _) :-
+  couleur(X).
 
-testFM(Houses) :-
-  member(maison(1, _, norvegien, _, _, _), Houses).
+valeurVersItems(X, _, X, _, _, _) :-
+  nationalite(X).
 
-creerFaitsMaisons :-
+valeurVersItems(X, _, _, X, _, _) :-
+  animal(X).
+
+valeurVersItems(X, _, _, _, X, _) :-
+  breuvage(X).
+
+valeurVersItems(X, _, _, _, _, X) :-
+  met(X).
+
+tupleVersItems((X, Y), X, Y, _, _, _) :-
+  couleur(X), nationalite(Y).
+
+tupleVersItems((X, Y), X, _, Y, _, _) :-
+  couleur(X), animal(Y).
+
+tupleVersItems((X, Y), X, _, _, Y, _) :-
+  couleur(X), breuvage(Y).
+
+tupleVersItems((X, Y), X, _, _, _, Y) :-
+  couleur(X), met(Y).
+
+tupleVersItems((X, Y), _, X, Y, _, _) :-
+  nationalite(X), animal(Y).
+
+tupleVersItems((X, Y), _, X, _, Y, _) :-
+  nationalite(X), breuvage(Y).
+
+tupleVersItems((X, Y), _, X, _, _, Y) :-
+  nationalite(X), met(Y).
+
+tupleVersItems((X, Y), _, _, X, Y, _) :-
+  animal(X), breuvage(Y).
+
+tupleVersItems((X, Y), _, _, X, _, Y) :-
+  animal(X), met(Y).
+
+tupleVersItems((X, Y), _, _, _, X, Y) :-
+  breuvage(X), met(Y).
+
+tupleVersItems((Y, X), X, Y, _, _, _) :-
+  couleur(X), nationalite(Y).
+
+tupleVersItems((Y, X), X, _, Y, _, _) :-
+  couleur(X), animal(Y).
+
+tupleVersItems((Y, X), X, _, _, Y, _) :-
+  couleur(X), breuvage(Y).
+
+tupleVersItems((Y, X), X, _, _, _, Y) :-
+  couleur(X), met(Y).
+
+tupleVersItems((Y, X), _, X, Y, _, _) :-
+  nationalite(X), animal(Y).
+
+tupleVersItems((Y, X), _, X, _, Y, _) :-
+  nationalite(X), breuvage(Y).
+
+tupleVersItems((Y, X), _, X, _, _, Y) :-
+  nationalite(X), met(Y).
+
+tupleVersItems((Y, X), _, _, X, Y, _) :-
+  animal(X), breuvage(Y).
+
+tupleVersItems((Y, X), _, _, X, _, Y) :-
+  animal(X), met(Y).
+
+tupleVersItems((Y, X), _, _, _, X, Y) :-
+  breuvage(X), met(Y).
+
+verifierFaitsMaisons(Houses) :-
+  length(Houses, 5),
+  maisonsToutesDiff(Houses),
+  verifierFaitsMemeMaison(Houses),
+  verifierFaitsVoisin(Houses),
+  verifierFaitsGauche(Houses),
+  verifierFaitsDroite(Houses),
+  verifierCouleurs(Houses),
+  verifierNationalites(Houses),
+  verifierAnimals(Houses),
+  verifierBreuvages(Houses),
+  verifierMets(Houses).
+
+verifierFaitsVoisin(H) :-
+  findall((X, Y), voisin(X, Y), VS),
+  maplist(verifierFaitVoisin(H), VS).
+
+verifierFaitsMemeMaison(H) :-
   findall((X, Y), meme(X, Y), XS),
-  maplist(creerFaitMaisonSansOrdre, XS),
-  findall((X, Y), voisin(X, Y), VS).
+  maplist(verifierFaitMemeMaison(H), XS).
 
-creerFaitMaisonSansOrdre((X, Y)) :-
-  (creerFaitMaison((X, Y)), !);
-  (creerFaitMaison((Y, X)), !).
+verifierFaitsGauche(H) :-
+  findall((X, Y), gauche(X, Y), XS),
+  maplist(verifierFaitGauche(H), XS).
 
-creerFaitMaison((X, Y)) :-
+verifierFaitsDroite(H) :-
+  findall((X, Y), droite(X, Y), XS),
+  maplist(verifierFaitDroite(H), XS).
+
+verifierCouleurs(H) :-
+    findall((X), couleur(X), XS),
+    maplist(verifierCouleur(H), XS).
+
+verifierNationalites(H) :-
+  findall((X), nationalite(X), XS),
+  maplist(verifierNationalite(H), XS).
+
+verifierAnimals(H) :-
+  findall((X), animal(X), XS),
+  maplist(verifierAnimal(H), XS).
+
+verifierBreuvages(H) :-
+  findall((X), breuvage(X), XS),
+  maplist(verifierBreuvage(H), XS).
+
+verifierMets(H) :-
+  findall((X), met(X), XS),
+  maplist(verifierMet(H), XS).
+
+verifierFaitVoisin(H, (X, Y)) :-
   position(X),
-  couleur(Y),
-  assert(faitsMaisons(X, Y, _, _, _, _)).
+  valeurVersItem(Y, C, N, A, B, M),
+  PY is X+1,
+  nth1(PY, H, maison(C, N, A, B, M)).
 
-creerFaitMaison((X, Y)) :-
+verifierFaitVoisin(H, (Y, X)) :-
   position(X),
-  nationalite(Y),
-  assert(faitsMaisons(X, _, Y, _, _, _)).
+  valeurVersItem(Y, C, N, A, B, M),
+  PY is X+1,
+  nth1(PY, H, maison(C, N, A, B, M)).
 
-creerFaitMaison((X, Y)) :-
+verifierFaitVoisin(H, (X, Y)) :-
+  valeurVersItems(X, CX, NX, AX, BX, MX),
+  valeurVersItems(Y, CY, NY, AY, BY, MY),
+  append(_, [maison(CX, NX, AX, BX, MX), maison(CY, NY, AY, BY, MY) | _], H).
+
+verifierFaitVoisin(H, (Y, X)) :-
+  valeurVersItems(X, CX, NX, AX, BX, MX),
+  valeurVersItems(Y, CY, NY, AY, BY, MY),
+  append(_, [maison(CX, NX, AX, BX, MX), maison(CY, NY, AY, BY, MY) | _], H).
+
+verifierFaitMemeMaison(H, (X, Y)) :-
   position(X),
-  animal(Y),
-  assert(faitsMaisons(X, _, _, Y, _, _)).
+  valeurVersItems(Y, C, N, A, B, M),
+  nth1(X, H, maison(C, N, A, B, M)).
 
-creerFaitMaison((X, Y)) :-
+verifierFaitMemeMaison(H, (Y, X)) :-
   position(X),
-  breuvage(Y),
-  assert(faitsMaisons(X, _, _, _, Y, _)).
+  valeurVersItems(Y, C, N, A, B, M),
+  nth1(X, H, maison(C, N, A, B, M)).
 
-creerFaitMaison((X, Y)) :-
+verifierFaitMemeMaison(H, T) :-
+  tupleVersItems(T, C, N, A, B, M),
+  member(maison(C, N, A, B, M), H).
+
+verifierFaitGauche(H, (X, Y)) :-
   position(X),
-  met(Y),
-  assert(faitsMaisons(X, _, _, _, _, Y)).
+  valeurVersItems(Y, C, N, A, B, M),
+  nth1(PY, H, maison(C, N, A, B, M)),
+  X < PY.
 
-creerFaitMaison((X, Y)) :-
-  couleur(X),
-  nationalite(Y),
-  assert(faitsMaisons(_, X, Y, _, _, _)).
+verifierFaitGauche(H, (Y, X)) :-
+  position(X),
+  valeurVersItems(Y, C, N, A, B, M),
+  nth1(PY, H, maison(C, N, A, B, M)),
+  X < PY.
 
-creerFaitMaison((X, Y)) :-
-  couleur(X),
-  animal(Y),
-  assert(faitsMaisons(_, X, _, Y, _, _)).
+verifierFaitGauche(H, (X, Y)) :-
+  valeurVersItems(X, C1, N1, A1, B1, M1),
+  valeurVersItems(Y, C2, N2, A2, B2, M2),
+  nth1(PX, H, maison(C1, N1, A1, B1, M1)),
+  nth1(PY, H, maison(C2, N2, A2, B2, M2)),
+  PX < PY.
 
-creerFaitMaison((X, Y)) :-
-  couleur(X),
-  breuvage(Y),
-  assert(faitsMaisons(_, X, _, _, Y, _)).
+verifierFaitDroite(H, (X, Y)) :-
+  position(X),
+  valeurVersItems(Y, C, N, A, B, M),
+  nth1(PY, H, maison(C, N, A, B, M)),
+  X > PY.
 
-creerFaitMaison((X, Y)) :-
-  couleur(X),
-  met(Y),
-  assert(faitsMaisons(_, X, _, _, _, Y)).
+verifierFaitDroite(H, (Y, X)) :-
+  position(X),
+  valeurVersItems(Y, C, N, A, B, M),
+  nth1(PY, H, maison(C, N, A, B, M)),
+  X > PY.
 
-creerFaitMaison((X, Y)) :-
-  nationalite(X),
-  animal(Y),
-  assert(faitsMaisons(_, _, X, Y, _, _)).
+verifierFaitDroite(H, (X, Y)) :-
+  valeurVersItems(X, C1, N1, A1, B1, M1),
+  valeurVersItems(Y, C2, N2, A2, B2, M2),
+  nth1(PX, H, maison(C1, N1, A1, B1, M1)),
+  nth1(PY, H, maison(C2, N2, A2, B2, M2)),
+  PX > PY.
 
-creerFaitMaison((X, Y)) :-
-  nationalite(X),
-  breuvage(Y),
-  assert(faitsMaisons(_, _, X, _, Y, _)).
+verifierCouleur(H, X) :-
+  member(maison(X, _, _, _, _), H).
 
-creerFaitMaison((X, Y)) :-
-  nationalite(X),
-  met(Y),
-  assert(faitsMaisons(_, _, X, _, _, Y)).
+verifierNationalite(H, X) :-
+  member(maison(_, X, _, _, _), H).
 
-creerFaitMaison((X, Y)) :-
-  animal(X),
-  breuvage(Y),
-  assert(faitsMaisons(_, _, _, X, Y, _)).
+verifierAnimal(H, X) :-
+  member(maison(_, _, X, _, _), H).
 
-creerFaitMaison((X, Y)) :-
-  animal(X),
-  met(Y),
-  assert(faitsMaisons(_, _, _, X, _, Y)).
+verifierBreuvage(H, X) :-
+  member(maison(_, _, _, X, _), H).
 
-creerFaitMaison((X, Y)) :-
-  breuvage(X),
-  met(Y),
-  assert(faitsMaisons(_, _, _, _, X, Y)).
+verifierMet(H, X) :-
+  member(maison(_, _, _, _, X), H).
 
-question( Position, Couleur, Nationalite, Animal, Breuvage, Mets ) :-
-	maison(Position, Couleur, Nationalite, Animal, Breuvage, Mets),
-  memeMaison(Position, Couleur),
-  memeMaison(Position, Nationalite),
-  memeMaison(Position, Animal),
-  memeMaison(Position, Breuvage),
-  memeMaison(Position, Mets).
+maisonsToutesDiff([maison(C1, N1, A1, B1, M1), maison(C2, N2, A2, B2, M2), maison(C3, N3, A3, B3, M3), maison(C4, N4, A4, B4, M4), maison(C5, N5, A5, B5, M5)]) :-
+  tousDiff([C1, C2, C3, C4, C5]),
+  tousDiff([N1, N2, N3, N4, N5]),
+  tousDiff([A1, A2, A3, A4, A5]),
+  tousDiff([B1, B2, B3, B4, B5]),
+  tousDiff([M1, M2, M3, M4, M5]).
 
 tousDiff([]).
 tousDiff(XS) :-
@@ -237,3 +288,7 @@ tousDiff(XS) :-
    length(XS, L1),
    length(SS, L2),
    L1 = L2.
+
+question( Position, Couleur, Nationalite, Animal, Breuvage, Mets ) :-
+  verifierFaitsMaisons(Houses),
+  nth1(Position, Houses, maison(Couleur, Nationalite, Animal, Breuvage, Mets)).
